7cb5047ca6f66f246457b88484e57bfbf4ac46a9
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-09-23 22:18:38 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    uInput: "",
+    uPattern: 0,
+  });
+  const [patterns, setPatterns] = useState(null);
+
+  const handleChange = (e) =>
+    setUserInput({
+      ...userInput,
+      [e.target.name]: e.target.value,
+    });
+
+  const { uInput, uPattern } = userInput;
+
+  function findPattern(input, patternLength) {
+    console.log("foundPatterns");
+    if (
+      patternLength === null ||
+      input === null ||
+      typeof patternLength === "undefined" ||
+      typeof input === "undefined"
+    ) {
+      return `Neither input or patternLength can be null or undefined`;
+    }
+
+    if (typeof input !== "string") {
+      return `input ${input} is not a string`;
+    }
+
+    if (typeof patternLength !== "number") {
+      return `patternLength ${patternLength} is not a number`;
+    }
+
+    if (patternLength < 0) {
+      return `patternLength can't be negative, was: ${patternLength}`;
+    }
+
+    if (input.legth < patternLength) {
+      return `input length can't be smaller than pattern length`;
+    }
+
+    const foundPatterns = {};
+    for (let i = 0; i < input.length - (patternLength - 1); i++) {
+      let pattern = input.substring(i, i + patternLength);
+      const escapedPattern = pattern.replace(
+        /[-[\]{}()*+?.,\\^$|#\s]/g,
+        "\\$&"
+      );
+      const reg = RegExp(escapedPattern, "g");
+      let occurrences = (input.match(reg) || []).length;
+      if (occurrences > 1 && !Object.keys(foundPatterns).includes(pattern))
+        foundPatterns[pattern] = occurrences;
+    }
+    console.log(foundPatterns);
+    return setPatterns(foundPatterns);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>String Pattern: </label>
+      <input
+        onChange={(e) => {
+          handleChange(e);
+        }}
+        type='text'
+        id='str'
+        value={uInput}
+        name='uInput'
+      />{" "}
+      <br />
+      <label>Pattern Length: </label>
+      <input
+        onChange={(e) => {
+          handleChange(e);
+        }}
+        type='number'
+        id='freq'
+        value={uPattern}
+        name='uPattern'
+      />{" "}
+      <br />
+      <button
+        id='b'
+        className='myButton'
+        onClick={(e) => findPattern(uInput, uPattern)}
+      >
+        <span>Result</span>
+      </button>
+      <hr />
+      {/* <p>{patterns && <span> {patterns} </span>}</p> */}
+      <p>
+        <span id='display'>{patterns}</span>
+      </p>
+    </>
   );
 }
 
